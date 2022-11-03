f2506065842d0e1df869e7e6d922b7b790ffe862
varunchaudhari <varun.chaudhari1@gmail.com>
2022-11-03 22:01:24 +0530

    Revert "minor changes"
    This reverts commit 83d92182b1b2d07cf9e053dd3e653a1a7da02c6e.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,11 @@
 import logging
 
-import uvicorn
-
-from manage import migrate, upgrade
 from .controller.api import app
 from .database import init_db
 
 
 @app.on_event("startup")
 async def on_startup():
-    migrate()
-    upgrade()
     await init_db()
 
 
@@ -18,5 +13,3 @@ async def on_startup():
 async def shutdown():
     logging.info(msg='Server Shutting down')
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
